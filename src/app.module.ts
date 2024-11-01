54d0207c8a373aa3df4af29f5a94fbff8455df26
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2024-11-01 22:45:30 +0630

    sip - add validation schema for env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { PostsModule } from './posts/posts.module';
 import { TagsModule } from './tags/tags.module';
@@ -24,7 +25,8 @@ const ENV = process.env.NODE_ENV
             load: [
                 appConfig,
                 databaseConfig
-            ]
+            ],
+            validationSchema: environmentValidation
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
