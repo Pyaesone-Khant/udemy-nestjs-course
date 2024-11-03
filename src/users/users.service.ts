1756e69521e7f92f4263b2830743804a360db337
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2024-11-03 09:55:16 +0630

    sip - add built-in exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,7 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
+import { BadRequestException, forwardRef, Inject, Injectable, RequestTimeoutException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { Repository } from 'typeorm';
-import profileConfig from './config/profile.config';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './user.entity';
 
@@ -21,16 +19,18 @@ export class UsersService {
         private readonly authService: AuthService,
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
-        @Inject(profileConfig.KEY)
-        private readonly profileConfiguration: ConfigType<typeof profileConfig>
+
+        /**
+         * specific custom config file for user service
+         */
+        // @Inject(profileConfig.KEY)
+        // private readonly profileConfiguration: ConfigType<typeof profileConfig>
     ) { }
 
     /**
      * The method to get all Users.
      */
     public async findAll() {
-
-        return this.profileConfiguration;
 
         let users = await this.userRepository.find();
         return users;
@@ -40,22 +40,46 @@ export class UsersService {
      * The method to create a new User.
      */
     public async create(createUserDto: CreateUserDto) {
-        // check if user exists or not
-        const existingUser = this.userRepository.findOne({
-            where: {
-                email: createUserDto.email
-            }
-        })
+
+        let existingUser = undefined;
+
+        try {
+            // check if user exists or not
+            existingUser = await this.userRepository.findOne({
+                where: {
+                    email: createUserDto.email
+                }
+            })
+        } catch (error) {
+            throw new RequestTimeoutException(
+                'Unable to process your request at the moment, please try again later!',
+                {
+                    description: 'Error connecting to database!'
+                }
+            )
+        }
 
         // if user exists, throw error (Handle exception)
         if (existingUser) {
-
+            throw new BadRequestException(
+                'User with this email already exists!',
+            )
         }
 
         // create user
         let newUser = this.userRepository.create(createUserDto)
-        newUser = await this.userRepository.save(newUser)
-        return newUser
+        try {
+            newUser = await this.userRepository.save(newUser)
+        } catch (error) {
+            throw new RequestTimeoutException(
+                'Unable to process your request at the moment, please try again later!',
+                {
+                    description: 'Error connecting to database!'
+                }
+            )
+        }
+
+        return newUser;
     }
 
     /**
@@ -63,9 +87,26 @@ export class UsersService {
      */
     public async findOne(id: number) {
 
-        let user = await this.userRepository.findOne({
-            where: { id }
-        });
+        let user = undefined;
+
+        try {
+            user = await this.userRepository.findOne({
+                where: { id }
+            });
+        } catch (error) {
+            throw new RequestTimeoutException(
+                'Unable to process your request at the moment, please try again later!',
+                {
+                    description: 'Error connecting to database!'
+                }
+            )
+        }
+
+        if (!user) {
+            throw new BadRequestException(
+                'User not found!',
+            )
+        }
 
         return user;
     }
