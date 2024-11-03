4ebd4ad642075db2a3b1efbd0d30b9a7eb5981a2
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2024-11-03 21:34:03 +0630

    sip - add & apply access-token guard in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { AccessTokenGuard } from 'src/auth/guards/access-token/access-token.guard';
 import { CreateManyUsersDto } from './dtos/create-many-users.dto';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
@@ -46,6 +47,7 @@ export class UsersController {
         return this.usersService.create(createUserDto);
     }
 
+    @UseGuards(AccessTokenGuard)
     @Post('create-many')
     createMany(@Body() createManyUsersDto: CreateManyUsersDto) {
         return this.usersService.createMany(createManyUsersDto)
