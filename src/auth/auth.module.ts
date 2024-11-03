bc6f877df80c9148e48518d822fe50d77a96cdef
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2024-11-03 20:51:13 +0630

    sip - add jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import jwtConfig from './config/jwt.config';
 import { AuthService } from './providers/auth.service';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
@@ -8,12 +11,18 @@ import { SignInProvider } from './providers/sign-in.provider';
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService, {
-        provide: HashingProvider, // abstract class
-        useClass: BcryptProvider
-    }, SignInProvider],
+    providers: [
+        AuthService,
+        {
+            provide: HashingProvider, // abstract class
+            useClass: BcryptProvider
+        },
+        SignInProvider
+    ],
     imports: [
-        forwardRef(() => UsersModule)
+        forwardRef(() => UsersModule),
+        ConfigModule.forFeature(jwtConfig),
+        JwtModule.registerAsync(jwtConfig.asProvider())
     ],
     exports: [AuthService, HashingProvider]
 })
