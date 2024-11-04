4fdd9d043624b2f1e112e159010519ef3f822579
Pyaesone Khant <pyaepsk@gmail.com>
2024-11-04 09:40:44 +0630

    sip - add validation for jwt env

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -9,5 +9,9 @@ export default Joi.object({
     DB_USERNAME: Joi.string().required(),
     DB_PASSWORD: Joi.string().required(),
     DB_NAME: Joi.string().required(),
-    PROFILE_API_KEY: Joi.string().required()
+    PROFILE_API_KEY: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    JWT_ACCESS_TOKNE_TTL: Joi.number().required().default(3600),
+    JWT_TOKEN_AUDIENCE: Joi.string().required(),
+    JWT_TOKEN_ISSUER: Joi.string().required(),
 })
