51f305a90326cd868c1e346a0d71bc627957584b
Pyaesone Khant <pyaepsk@gmail.com>
2024-11-05 15:01:14 +0630

    sip - add create-google-user method

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,7 +1,11 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, OnModuleInit, RequestTimeoutException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { OAuth2Client } from 'google-auth-library';
 import jwtConfig from 'src/auth/config/jwt.config';
+import { GenerateTokenProvider } from 'src/auth/providers/generate-token.provider';
+import { UsersService } from 'src/users/providers/users.service';
+import { User } from 'src/users/user.entity';
+import { GoogleTokenDto } from '../dtos/google-token.dto';
 
 
 @Injectable()
@@ -11,7 +15,12 @@ export class GoogleAuthenticationService implements OnModuleInit {
 
     constructor(
         @Inject(jwtConfig.KEY)
-        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+
+        @Inject(forwardRef(() => UsersService))
+        private readonly usersService: UsersService,
+
+        private readonly generateTokenProvider: GenerateTokenProvider
     ) { }
 
     // This method will be called when (the module is initialized || the app start) || It will create a new OAuth2Client instance as soon as (the module is initialized || the app start)
@@ -20,6 +29,32 @@ export class GoogleAuthenticationService implements OnModuleInit {
         const clientSecret = this.jwtConfiguration.googleClientSecret;
 
         this.oAuthClient = new OAuth2Client(clientId, clientSecret)
+    }
+
+    public async authenticate(googleTokenDto: GoogleTokenDto) {
+        let user: User | undefined = undefined;
+
+        // verify the google token sent by the user
+        const ticket = await this.oAuthClient.verifyIdToken({
+            idToken: googleTokenDto.token,
+        });
+
+        const { email, sub: googleId, given_name: firstName, family_name: lastName } = ticket.getPayload();
+
+        // Find a user by googleId
+        try {
+            user = await this.usersService.findOneByGoogleId(googleId)
+        } catch (error) {
+            throw new RequestTimeoutException(error)
+        }
+
+        // If the user exist, generate tokens & return them
+        if (user) {
+            return this.generateTokenProvider.generateToken(user)
+        }
+
+        // Create a new user
+
     }
 
 }
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -4,8 +4,11 @@ import { AuthService } from 'src/auth/providers/auth.service';
 import { Repository } from 'typeorm';
 import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { GoogleUser } from '../interfaces/google-user.interface';
 import { User } from '../user.entity';
+import { CreateGoogleUserProvider } from './create-google-user.provider';
 import { CreateUserProvider } from './create-user.provider';
+import { FindOneByGoogleIdProvider } from './find-one-by-google-id.provider';
 import { FindUserByEmailProvider } from './find-user-by-email.provider';
 import { UsersCreateManyProvider } from './users-create-many.provider';
 
@@ -37,7 +40,17 @@ export class UsersService {
         /**
          * Inject findUserByEmailProvider
          */
-        private readonly findUserByEmailProvider: FindUserByEmailProvider
+        private readonly findUserByEmailProvider: FindUserByEmailProvider,
+
+        /**
+         * Inject findUserByGoogleIdProvider
+         */
+        private readonly findOneByGoogleIdProvider: FindOneByGoogleIdProvider,
+
+        /**
+         * Inject createGoogleUserProvider
+         */
+        private readonly createGoogleUserProvider: CreateGoogleUserProvider,
 
         /**
          * specific custom config file for user service
@@ -102,6 +115,20 @@ export class UsersService {
      * The method to find user by email
      */
     public async findOneByEmail(email: string) {
-        return this.findUserByEmailProvider.findOneByEmail(email)
+        return await this.findUserByEmailProvider.findOneByEmail(email)
+    }
+
+    /**
+     * The method to find user by google id
+     */
+    public async findOneByGoogleId(googleId: string) {
+        return await this.findOneByGoogleIdProvider.findOneByGoogleId(googleId)
+    }
+
+    /**
+     * The method to create a new Google User.
+     */
+    public async createGoogleUser(googleUser: GoogleUser) {
+        return await this.createGoogleUserProvider.createGoogleUser(googleUser)
     }
 }
