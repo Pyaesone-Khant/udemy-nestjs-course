42793c8835d3b6b2a9cedd483e69291e0d562ea9
Pyaesone Khant <pyaepsk@gmail.com>
2024-11-05 11:07:29 +0630

    sip - initialize google oAuthClient

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from 'src/auth/config/jwt.config';
+
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+
+    private oAuthClient: OAuth2Client;
+
+    constructor(
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
+    ) { }
+
+    // This method will be called when (the module is initialized || the app start) || It will create a new OAuth2Client instance as soon as (the module is initialized || the app start)
+    onModuleInit() {
+        const clientId = this.jwtConfiguration.googleClientId;
+        const clientSecret = this.jwtConfiguration.googleClientSecret;
+
+        this.oAuthClient = new OAuth2Client(clientId, clientSecret)
+    }
+
+}
